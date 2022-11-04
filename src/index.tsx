2e3f6db216c542719117ea87c36d1944ffe60f89
timur.gabdullin <timur.gabdullin@myoffice.team>
2022-11-05 01:29:04 +0300

    disable hide feature(dev stand) for prod build

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@ import "./index.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+const development =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
 const hide = false;
 const isPublic = window.location.hostname !== "localhost";
-const needHide = !(!isPublic || !hide);
+const needHide = !(!isPublic || !hide) && development;
 root.render(
   <React.StrictMode>
     {needHide ? (
       <div style={{ textAlign: "center" }}>
-        <img alt="allo" src="./gorin.png" style={{ height: "95vh" }} />
+        <img alt="allo" src="./gorin.png" style={{ width: "100%" }} />
       </div>
     ) : (
       <App />
